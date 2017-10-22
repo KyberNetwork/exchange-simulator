ca50e1dc93c0910ab7cc3b9c6d3dd72b88ed8358
yaron velner <yaron172@yahoo.com>
2017-10-22 19:09:29 +0300

    restful api

---
+++ exchange_api_interface.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python3
+class ExchangeApiInterface:
+    def parse_trade_args(self, args):
+        pass
+    def parse_get_balance_args(self, args):
+        pass
+    def parse_withdraw_args(self, args):
+        pass
+    def parse_deposit_args(self, args):
+        pass
+
+
+################################################################################
+
+class TradeParams:
+    def __init__(self, api_key, src_token, dst_token, qty, rate ):
+        self.api_key = api_key
+        self.src_token = src_token
+        self.dst_token = dst_token
+        self.qty = qty
+        self.rate = rate
+
+
+################################################################################
+
+class GetBalanceParams:
+    def __init__(self, api_key, token ):
+        self.api_key = api_key
+        self.token = token
+
+################################################################################
+
+class DepositParams:
+    def __init__(self, api_key, token, qty ):
+        self.api_key = api_key
+        self.token = token
+        self.qty = qty
+
+################################################################################
+
+class WithdrawParams:
+    def __init__(self, api_key, token, qty, dst_address ):
+        self.api_key = api_key
+        self.token = token
+        self.qty = qty
+        self.dst_address = dst_address
+
+
+################################################################################
+
+class LiquiApiInterface(ExchangeApiInterface):
+    def __init__(self):
+        self.name = "Liqui"
+    def prase_trade_args(self, args):
+        # TODO
+        pass
+    def parse_get_balance_args(self, args):
+        # TODO
+        pass
+    def parse_withdraw_args(self, args):
+        # TODO
+        pass
+    def parse_deposit_args(self, args):
+        # TODO
+        pass
+
+################################################################################
---
+++ restful_api.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+from flask import Flask, request, jsonify
+from flask_restful import Resource, Api
+import json
+from enum import Enum
+
+import exchange_api_interface
+
+app = Flask(__name__)
+api = Api(app)
+
+
+class Employees(Resource):
+    def get(self):
+        return {'employees': 'zelda'}
+    def post(self):
+        print ("hello")
+        print (request.args)
+
+class LiquiTrade(Resource):
+    def post(self):
+        args = exchange_interface.LiquiApiInterface().prase_trade_args(request.args)
+        if( args is None ):
+            raise ValueError('liqui trade args are in invalid format')
+
+
+
+api.add_resource(Employees, '/employees') # Route_1
+api.add_resource(Employees2, '/bittrex/employees') # Route_1
+api.add_resource(Tracks, '/tracks') # Route_2
+api.add_resource(Employees_Name, '/employees/<employee_id>') # Route_3
+
+
+if __name__ == '__main__':
+     app.run(port='5002')
