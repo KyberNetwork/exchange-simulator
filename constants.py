2f976600bae1ae1ef77758a1ec190e929fcdf33e
yaron velner <yaron172@yahoo.com>
2017-10-23 11:56:52 +0300

    pep8 format

--- constants.py
+++ constants.py
@@ -5,5 +5,5 @@ BANK_ADDRESS = 0x7ffdb79da310995b0d5778b87f69a1340b639266
 
 OREDER_BOOK_IP = "TODO"
 
-TOKEN_TO_ADDRESS = { "KNC" : 0xbd46bb7cf321b4acf0a703422f4c2dd69ad0dba0,
-                     "ETH" : 0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee }
+TOKEN_TO_ADDRESS = {"KNC": 0xbd46bb7cf321b4acf0a703422f4c2dd69ad0dba0,
+                    "ETH": 0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee}
--- test_exchange.py
+++ test_exchange.py
@@ -5,7 +5,9 @@ import exchange
 import exchange_api_interface
 import math
 
+
 class TestDeposit(unittest.TestCase):
+
     def test_deposit_after_reset(self):
         exchange.reset_db()
         deposit_params = exchange_api_interface.DepositParams("kyber_liqui",
@@ -68,12 +70,12 @@ class TestDeposit(unittest.TestCase):
         liqui_exchange.deposit(deposit_params)
 
         # execute order
-        trade_params = exchange_api_interface.TradeParams( "kyber_liqui",
-                                                           "KNC",
-                                                           "ETH",
-                                                           5,
-                                                           0.03)
-        ret_val = liqui_exchange.execute_trade( trade_params )
+        trade_params = exchange_api_interface.TradeParams("kyber_liqui",
+                                                          "KNC",
+                                                          "ETH",
+                                                          5,
+                                                          0.03)
+        ret_val = liqui_exchange.execute_trade(trade_params)
 
         # get ETH balance
         balance = liqui_exchange.get_user_balance("kyber_liqui", "ETH")
@@ -82,7 +84,6 @@ class TestDeposit(unittest.TestCase):
         # get KNC balance
         balance = liqui_exchange.get_user_balance("kyber_liqui", "KNC")
         self.assertEqual(balance, 6)
-
 
 
 if __name__ == '__main__':
