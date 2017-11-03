413770d479cba802584bf92f631ee6f23b3b4e98
NamNguyen <namnm1991@gmail.com>
2017-11-03 12:04:48 +0700

    refactor order book

--- exchange.py
+++ exchange.py
@@ -17,18 +17,17 @@ logger = logging.getLogger(constants.LOGGER_NAME)
 
 class Exchange:
 
-    def __init__(self, exchange_name, listed_tokens, db, order_book_ip,
+    def __init__(self, exchange_name, listed_tokens, db,
                  ethereum_deposit_address, ethereum_bank_address,
                  deposit_delay_in_secs):
         self.name = exchange_name
         self.listed_tokens = listed_tokens
         self.db = db
-        self.order_book_ip = order_book_ip
         self.deposit_address = ethereum_deposit_address
         self.bank_address = ethereum_bank_address
         self.deposit_delay_in_secs = deposit_delay_in_secs
         self.mutex = Lock()
-        self.order_book_cache = {}
+        self.order_books = {}
         self.processed_order_ids = set()
 
     def before_api(self, api_key):
@@ -59,57 +58,17 @@ class Exchange:
 
     def get_order_book(self, src_token, dest_token):
         """Find order book in cache using src_token and dest_token as the key
-        If the order book is not found or expired then we fetch order book
-        from data fetcher
+        If the order book is not found then we will create it
+        If the order book is expired then we reload it
         """
-        CACHE_TIME = 60 * 1
-
-        def expired(ob):
-            return (time.time() - float(ob['Timestamp'])) > CACHE_TIME
-
         pair = "{}_{}".format(src_token, dest_token)
-        order_book = self.order_book_cache.get(pair, {"Timestamp": 0})
-        if expired(order_book):
-            logger.info("Fetching order book for {}".format(pair))
-            order_book = self.fetch_order_book(src_token, dest_token)
-            self.order_book_cache[pair] = order_book
-        return order_book
-
-    def fetch_order_book(self, src_token, dest_token):
-        def set_order_id(o):
-            """Create Id for an order by hashing a string contain
-            source token, destination token, it's rate and quantity
-            """
-            keys = [src_token, dest_token, o['Rate'], o['Quantity']]
-            o['Id'] = hash('.'.join(map(str, keys)))
-
-        order_book = {
-            'BuyPrices': {},
-            'SellPrices': {},
-            'Timestamp': time.time()
-        }
-        # TODO change this host
-        # host = 'http://{}/prices/{}/{}'.format(constants.OREDER_BOOK_IP,
-        #                                        src_token,
-        #                                        dest_token)
-        # currently, data fetcher only support this
-        host = 'http://{}/prices/knc/eth'.format(self.order_book_ip)
-        try:
-            r = requests.get(host)
-            if r.status_code == requests.codes.ok:
-                data = r.json()
-                order_book = data['exchanges'].get(self.name, order_book)
-                for type in ['BuyPrices', 'SellPrices']:
-                    # sort the order by rate
-                    # descending for BuyPrices, ascending for SellPrices
-                    sorted(order_book[type], key=itemgetter(
-                        'Rate'), reverse=(type == 'SellPrices'))
-                    # set Id for orders so we can keep track process order
-                    [set_order_id(o) for o in order_book[type]]
-            logger.debug("Buy order: {}".format(order_book['BuyPrices'][:3]))
-            logger.debug("Sell order: {}".format(order_book['SellPrices'][:3]))
-        except requests.exceptions.RequestException:
-            logger.error('Cannot make request to get order book')
+        order_book = self.order_books.get(pair, None)
+        if not order_book:
+            order_book = OrderBook(src_token, dest_token, self.name)
+            self.order_books[pair] = order_book
+        else:
+            if order_book.expired():
+                order_book.reload()
         return order_book
 
     def execute_trade_api(self, trade_params):
@@ -123,25 +82,26 @@ class Exchange:
         if buy:
             src_qty = trade_params.qty * trade_params.rate
             if user_src_balance < src_qty:
-                return TradeOutput(True, "insuficient qty", 0)
+                # TODO change this code, it's ugly
+                return TradeOutput(True, "insuficient qty", 0, 0, 0, {})
+            else:
+                order_book = self.get_order_book(trade_params.dst_token,
+                                                 trade_params.src_token)
+                orders = order_book.sell_prices
         else:
             src_qty = trade_params.qty
             if user_src_balance < src_qty:
-                return TradeOutput(True, "insuficient qty", 0)
-
-        order_book = self.get_order_book(trade_params.src_token,
-                                         trade_params.dst_token)
-        if buy:
-            orders = order_book['SellPrices']
-        else:
-            orders = order_book['BuyPrices']
+                # TODO change this code, it's ugly
+                return TradeOutput(True, "insuficient qty", 0, 0, 0, {})
+            else:
+                order_book = self.get_order_book(trade_params.src_token,
+                                                 trade_params.dst_token)
+                orders = order_book.buy_prices
 
         traded_cost = 0
         traded_quantity = 0
         required_quantity = trade_params.qty
         for order in orders:
-            logger.debug("Process order: {}".format(order))
-
             id = order['Id']
             if id in self.processed_order_ids:
                 continue  # order is already processed, continue to next order
@@ -170,7 +130,18 @@ class Exchange:
                               trade_params.src_token, user_src_balance)
         self.set_user_balance(trade_params.api_key,
                               trade_params.dst_token, user_dst_balance)
-        return TradeOutput(False, "", 0)
+        received = traded_quantity
+        remains = required_quantity - received
+        if remains == 0:
+            order_id = 0
+        else:
+            order = {'Rate': trade_params.rate, 'Quantity': remains}
+            order_id = order_book.add_order(order, buy)
+        balances = {}
+        for token in self.listed_tokens:
+            balance = self.get_user_balance(trade_params.api_key, token)
+            balances[token] = balance
+        return TradeOutput(False, "", order_id, received, remains, balances)
 
     def deposit(self, api_key, token, qty):
         """
@@ -238,98 +209,72 @@ class Exchange:
 
 
 class OrderBook:
-    BUY = 'BuyPrices'
-    SELL = 'SellPrices'
+    MAX_ORDER_ID = 2 ** 31
 
-    def __init__(self, order_book_ip, source_token, dest_token, exchange_name):
-        source_token = source_token.upper()
-        dest_token = dest_token.upper()
-        for pair in constants.SUPPORTED_PAIR:
-            if source_token in pair and dest_token in pair:
-                if pair.startswith(source_token):
-                    self.price_type = OrderBook.BUY
-                    self.source_token = source_token
-                    self.dest_token = dest_token
-                else:
-                    self.price_type = OrderBook.SELL
-                    self.source_token = dest_token
-                    self.dest_token = source_token
-                break
+    def __init__(self, source_token, dest_token, exchange_name):
+        self.source_token = source_token
+        self.dest_token = dest_token
+        self.exchange_name = exchange_name
+        self.timestamp = time.time()
+        self.buy_prices = []
+        self.sell_prices = []
 
-        host = 'http://%s/prices/%s/%s' % (order_book_ip,
-                                           self.source_token,
-                                           self.dest_token)
-        self.load_orderbook(host.lower(), exchange_name.lower())
+        self.load_order_book(self.source_token, self.dest_token)
 
-    def load_orderbook(self, host, exchange_name):
-        def get_data(host, exchange_name):
-            # TODO: wait for new api from victor, current use old one
-            VALID_HTTP_RESPONSE = [200]
-            try:
-                r = requests.get(host)
-            except requests.exceptions.RequestException:
-                return {"Valid": False,
-                        "reason": "Can not make request to host"}
-            if r.status_code in VALID_HTTP_RESPONSE:
-                market_data = r.json()
-                if market_data.get("success", False):
-                    if "exchanges" in market_data:
-                        empty = {"Valid": True,
-                                 "BuyPrices": {},
-                                 "SellPrices": {}}
-                        return market_data["exchanges"].get(exchange_name,
-                                                            empty)
-                return {"Valid": False,
-                        "reason": market_data.get("reason",
-                                                  "Invalid server data format")
-                        }
-            return {"Valid": False, "reason": "Invalid server status code"}
+    def order_id(self, o):
+        """Create Id for an order by hashing a string contain
+        source token, destination token, it's rate and quantity
+        """
+        keys = [self.source_token, self.dest_token, o['Rate'], o['Quantity']]
+        return hash('.'.join(map(str, keys))) % OrderBook.MAX_ORDER_ID
 
-        def parse_orderbook(market_data):
-            data = []
-            if market_data["Valid"]:
-                reversed_sort = (self.price_type == OrderBook.BUY)
-                for command in market_data[self.price_type]:
-                    data.append((command['Rate'], command['Quantity']))
-                data.sort(key=itemgetter(0), reverse=reversed_sort)
-            return data
+    def expired(self):
+        CACHE_TIME = 60 * 1
+        return (time.time() - self.timestamp) > CACHE_TIME
 
-        market_data = get_data(host, exchange_name)
-        self.order_book = parse_orderbook(market_data)
+    def reload(self):
+        self.load_order_book()
 
-    def execute_trade(self, required_qty, request_rate):
-
-        def rate_exceed_threshold(rate, threshold):
-            return ((rate - threshold) *
-                    (-1 if self.price_type == OrderBook.BUY else 1)) > 0
-        if required_qty < 0:
-            return 0, 0
-        total_cost = 0
-        total_quantity = 0
-        for command in self.order_book:
-            command_rate, command_volume = command
-            if rate_exceed_threshold(command_rate, request_rate):
-                break
-            needed_volume = required_qty - total_quantity
-            if needed_volume > command_volume:
-                total_cost += command_rate * command_volume
-                total_quantity += command_volume
-            else:
-                total_cost += needed_volume * command_rate
-                total_quantity += needed_volume
-                break
-        if self.price_type == OrderBook.BUY:
-            return total_quantity, total_cost
+    def add_order(self, order, buy):
+        order["Id"] = self.order_id(order)
+        if buy:
+            self.buy_prices.append(order)
+            sorted(self.buy_prices, key=itemgetter('Rate'), reverse=False)
         else:
-            return total_cost, total_quantity
-###############################################################################
+            self.sell_prices.append(order)
+            sorted(self.sell_prices, key=itemgetter('Rate'), reverse=True)
+        return order["Id"]
+
+    def load_order_book(self, src_token, dest_token):
+        try:
+            host = 'http://{}/prices/{}/{}'.format(constants.OREDER_BOOK_IP,
+                                                   self.source_token,
+                                                   self.dest_token)
+            r = requests.get(host)
+            if r.status_code == requests.codes.ok:
+                data = r.json()
+                order_book = data['exchanges'][self.exchange_name]
+                for type in ['BuyPrices', 'SellPrices']:
+                    # sort the order by rate
+                    # descending for BuyPrices, ascending for SellPrices
+                    sorted(order_book[type], key=itemgetter(
+                        'Rate'), reverse=(type == 'SellPrices'))
+                    # set Id for orders so we can keep track process order
+                    for o in order_book[type]:
+                        o["Id"] = self.order_id(o)
+
+                self.buy_prices = order_book['BuyPrices']
+                self.sell_prices = order_book['SellPrices']
+                self.timestamp = float(order_book['Timestamp'])
+        except requests.exceptions.RequestException:
+            logger.error('Cannot make request to get order book')
 
 
 rdb = redis.Redis(host='localhost', port=6379, db=0)
 
 
 liqui = Exchange(
-    "liqui", [constants.KNC, constants.ETH], rdb, constants.OREDER_BOOK_IP,
+    "liqui", [constants.KNC, constants.ETH], rdb,
     constants.LIQUI_ADDRESS, constants.BANK_ADDRESS, 5 * 60)
 
 #
